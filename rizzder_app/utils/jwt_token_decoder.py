e4d0d9e9b517ae25d3508b7700feac3427984b0c
Teodora Plopeanu <teodora.plopeanu@gmail.com>
2024-12-18 12:12:30 +0200

    Jwt token decoding fix.

--- rizzder_app/utils/jwt_token_decoder.py
+++ rizzder_app/utils/jwt_token_decoder.py
@@ -1,6 +1,7 @@
 import jwt
 import logging
 from django.contrib.auth.models import User as authUser
+from django.conf import settings
 
 logger = logging.getLogger(__name__)
 
@@ -15,7 +16,7 @@ class JWTTokenDecoder():
 
     def getUserFromToken(self):
         from ..models import User
-        decodedPayload = jwt.decode(self.token, None, None)
+        decodedPayload = jwt.decode(self.token, settings.SECRET_KEY, algorithms=['HS256'])
         user_id = decodedPayload.get("user_id")
         auth_user = authUser.objects.get(id=user_id)
 
