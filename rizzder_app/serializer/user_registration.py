8d3c0a4a672a04cdb6901115e2e9f08d50bfa68b
Teodora <teodora.plopeanu@gmail.com>
2024-12-23 21:12:54 +0200

    SignUp page

--- rizzder_app/serializer/user_registration.py
+++ rizzder_app/serializer/user_registration.py
@@ -5,9 +5,15 @@ from django.contrib.auth.models import User
 class user_registration_serializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['username', 'password']
+        fields = ['username', 'password', 'email']
         extra_kwargs = {
             'password': {'write_only': True}
+        }
+
+        labels = {
+            'username': 'Enter your Username',
+            'password': 'Create a Password',
+            'email': 'Enter your Email',
         }
 
     def create(self, validated_data):
