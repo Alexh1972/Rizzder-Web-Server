b2a1d4c0ae1f332f4e719816316a008f6e3e91f2
Andrei Giurgiu <141114664+SkipperAndrei@users.noreply.github.com>
2025-01-07 02:13:27 +0200

    Update settings.py changed STATIC_ROOT and STATICFILES_DIRS

--- rizzder/settings.py
+++ rizzder/settings.py
@@ -186,7 +186,11 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "rizzder_app", "static")
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
