31494749d692b61e131e5523a8c39e4edb2259dc
Andrei Giurgiu <141114664+SkipperAndrei@users.noreply.github.com>
2025-01-08 17:38:27 +0200

    Changed asgi.py

--- rizzder/asgi.py
+++ rizzder/asgi.py
@@ -15,14 +15,12 @@ from channels.routing import ProtocolTypeRouter, URLRouter
 from rizzder_app import websocket_urlpatterns
 from channels.auth import AuthMiddlewareStack
 from django.core.asgi import get_asgi_application
-
+from channels.security.websocket import AllowedHostsOriginValidator
 application = ProtocolTypeRouter(
     {
         "http": get_asgi_application(),
-        "websocket": AuthMiddlewareStack(
-            URLRouter(
-                websocket_urlpatterns
-            )
-        )
+        "websocket": AllowedHostsOriginValidator(
+            AuthMiddlewareStack(URLRouter(websocket_urlpatterns))
+        ),
     }
 )
