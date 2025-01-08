3629dd94affa2a2f680b68284aef6058d174d322
Andrei Giurgiu <141114664+SkipperAndrei@users.noreply.github.com>
2025-01-08 15:21:15 +0200

    Update rounting.py

--- rizzder_app/rounting.py
+++ rizzder_app/rounting.py
@@ -2,5 +2,5 @@ from django.urls import path, include
 from .messaging import ChatConsumer
 
 websocket_urlpatterns = [
-    path("wss/<str:room_name>/", ChatConsumer.as_asgi()),
+    path("ws/<str:room_name>/", ChatConsumer.as_asgi()),
 ]
